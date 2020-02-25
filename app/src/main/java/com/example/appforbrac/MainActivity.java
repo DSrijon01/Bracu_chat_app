9c78121861c68e2329287c0896b7d3a487eed39a
Srijon Biswas <srijon.biswas17@gmail.com>
2020-02-26 00:35:16 +0600

    Initial commit

---
+++ app/src/main/java/com/example/appforbrac/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.appforbrac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Toolbar mToolbar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mToolbar=(Toolbar) findViewById(R.id.main_page_toolbar);
+       setSupportActionBar(mToolbar);
+       getSupportActionBar().setTitle("BRACU");
+
+
+
+    }
+
+}
