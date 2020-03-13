bea485d2046948830496d23aa9826483b5e74a03
Srijon Biswas <srijon.biswas17@gmail.com>
2020-03-13 11:44:31 +0600

    AppbarLayout

--- app/src/main/java/com/example/appforbrac/MainActivity.java
+++ app/src/main/java/com/example/appforbrac/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.appforbrac;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
 
 public class MainActivity extends AppCompatActivity {
 
 
     private Toolbar mToolbar;
+    private ViewPager myViewpager;
+    private TabLayout myTabLayout;
+    private TabsAcessorAdapter myTabsAccessorAdapter;
+
+
+
 
 
     @Override
@@ -21,6 +30,14 @@ public class MainActivity extends AppCompatActivity {
         mToolbar=(Toolbar) findViewById(R.id.main_page_toolbar);
        setSupportActionBar(mToolbar);
        getSupportActionBar().setTitle("BRACU");
+
+
+       myViewpager = (ViewPager) findViewById(R.id.main_tabs_pager);
+       myTabsAccessorAdapter = new TabsAcessorAdapter(getSupportFragmentManager());
+       myViewpager.setAdapter(myTabsAccessorAdapter);
+       myTabLayout = (TabLayout) findViewById(R.id.main_tabs);
+       myTabLayout.setupWithViewPager(myViewpager);
+
 
 
 
